77cc6ddbcf7ddc78cc9c303933d4012eabb477e6
fumokmm <fumo.kmm@gmail.com>
2012-11-11 00:03:41 +0900

    FizzBuzzのサンプルを追加（枠だけ）

---
+++ src/test/test/jp/jvquery/samples/FizzBuzzSample.java
@@ -0,0 +1,17 @@
+package test.jp.jvquery.samples;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class FizzBuzzSample {
+
+    @Ignore
+    @Test
+    public void fizzBuzz() {
+	// TODO $.range の実装
+//	$.range(1, 100).map(new Conv<Integer, String>(){
+//	});
+    }
+
+}
