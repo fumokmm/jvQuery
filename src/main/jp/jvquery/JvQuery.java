d979eb62b15d724ce8d61186ab9da73cfb1d26f0
fumokmm <fumo.kmm@gmail.com>
2012-11-17 08:23:05 +0900

    古いインタフェースの削除。

--- src/main/jp/jvquery/JvQuery.java
+++ src/main/jp/jvquery/JvQuery.java
@@ -460,66 +460,6 @@ public final class JvQuery {
 
     public static interface FoldBlock<T, R> extends Function2<R, T, R> {}
     
-    public static interface Act<A> extends Act1<A> {
-    }
-
-    public static interface Act1<A1> {
-	public void run(A1 a1);
-    }
-
-    public static interface Act2<A1, A2> {
-	public void run(A1 a1, A2 a2);
-    }
-
-    public static interface Act3<A1, A2, A3> {
-	public void run(A1 a1, A2 a2, A3 a3);
-    }
-
-    public static interface Func<A, R> extends Func1<A, R> {
-    }
-
-    public static interface Func1<A1, R> {
-	public R call(A1 a1);
-    }
-
-    public static interface Func2<A1, A2, R> {
-	public R call(A1 a1, A2 a2);
-    }
-
-    public static interface Func3<A1, A2, A3, R> {
-	public R call(A1 a1, A2 a2, A3 a3);
-    }
-
-    public static interface Pred<A> extends Pred1<A> {
-    }
-
-    public static interface Pred1<A1> {
-	public boolean is(A1 a1);
-    }
-
-    public static interface Pred2<A1, A2> {
-	public boolean is(A1 a1, A2 a2);
-    }
-
-    public static interface Pred3<A1, A2, A3> {
-	public boolean is(A1 a1, A2 a2, A3 a3);
-    }
-
-    public static interface Conv<A, R> extends Conv1<A, R> {
-    }
-
-    public static interface Conv1<A1, R> {
-	public R convert(A1 a1);
-    }
-
-    public static interface Conv2<A1, A2, R> {
-	public R convert(A1 a1, A2 a2);
-    }
-
-    public static interface Conv3<A1, A2, A3, R> {
-	public R convert(A1 a1, A2 a2, A3 a3);
-    }
-    
     public static interface Filter {
 	<T> FilterWithIndexBlock<T> getFilter();
     }
